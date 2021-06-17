5613844aab15b200c4d27815665b61df269214af
Nazarii Hutsuliak <nazaripeople@gmail.com>
2021-06-17 19:07:52 +0300

    v.1.2

--- Calculation.Lib.Tests/CalcLibTests.cs
+++ Calculation.Lib.Tests/CalcLibTests.cs
@@ -2,62 +2,69 @@ using NUnit.Framework;
 
 namespace Calculation.Lib.Tests
 {
-        [TestFixture]
-        public class CalcLibTests
+    [TestFixture]
+    public static class CalcLibTests
+    {
+        [Test]
+        public static void Sum_20_and_30_Result_50()
         {
-            [Test]
-            public void Sum_20and30_Returned50()
-            {
-                int x = 20;
-                int y = 30;
-                int expected = 50;
-
-                CalcLib c = new CalcLib();
-                int actual = c.Sum(x, y);
-
-                Assert.AreEqual(expected, actual);
-
-            }
-            [Test]
-            public void Dif_40and50_Returned_10()
-            {
-                int x = 40;
-                int y = 50;
-                int expected = -10;
-
-                CalcLib c = new CalcLib();
-                int actual = c.Dif(x, y);
-
-                Assert.AreEqual(expected, actual);
-
-            }
-            [Test]
-            public void Div_40and20_Returned2()
-            {
-                int x = 40;
-                int y = 20;
-                int expected = 2;
-
-                CalcLib c = new CalcLib();
-                int actual = c.Div(x, y);
-
-                Assert.AreEqual(expected, actual);
-
-            }
-            [Test]
-            public void Mult_2and20_Returned40()
-            {
-                int x = 2;
-                int y = 20;
-                int expected = 40;
-
-                CalcLib c = new CalcLib();
-                int actual = c.Mult(x, y);
-
-                Assert.AreEqual(expected, actual);
-
-            }
+            // Arrange
+            const int expected = 50;
+            // Act
+            var actual = CalcLib.Addition(20, 30);
+            // Assert
+            Assert.AreEqual(expected, actual);
 
         }
-    
+        [Test]
+        public static void Difference_40_and_50_Result_minus_10()
+        {
+            // Arrange
+            const int expected = -10;
+            // Act
+            var actual = CalcLib.Substraction(40, 50);
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public static void Division_3_to_2_Result_1_point_5()
+        {
+            //Arrange
+            const double expected = 1.5;
+            //Act
+            var actual = CalcLib.Division(3, 2);
+            
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public static void Division_40and20_Returned2()
+        {
+            //Arrange
+            const int expected = 2;
+            //Act
+            var actual = CalcLib.Division(40, 20);
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public static void Multiplication_2and20_Returned40()
+        {
+            //Arrange
+            const int expected = 40;
+            //Act
+            int actual = CalcLib.Multiplication(2, 20);
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+
 }
